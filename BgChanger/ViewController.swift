46a6f427daeac8382c09c46fb6042f79c39df62c
PaMeSa <pakomedina@gmail.com>
2019-05-16 22:09:14 -0500

    Initial Commit

---
+++ BgChanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BgChanger
+//
+//  Created by Pako Medina on 16/05/2019.
+//  Copyright © 2019 Pako Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
